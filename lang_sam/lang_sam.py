fa7a78be341bf63f0be1a0dcaf41dff64ed28d93
Annie Xie <axie0906@gmail.com>
2023-12-05 12:05:23 -0800

    Add batched version of predict

--- lang_sam/lang_sam.py
+++ lang_sam/lang_sam.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 from groundingdino.models import build_model
 from groundingdino.util import box_ops
-from groundingdino.util.inference import predict
+from groundingdino.util.inference import predict, predict_batch
 from groundingdino.util.slconfig import SLConfig
 from groundingdino.util.utils import clean_state_dict
 from huggingface_hub import hf_hub_download
@@ -100,6 +100,19 @@ class LangSAM():
 
         return boxes, logits, phrases
 
+    def predict_dino_batch(self, image_pils, text_prompts, box_threshold, text_threshold):
+        image_trans = [transform_image(image_pil) for image_pil in image_pils]
+        boxes, logits, phrases = predict_batch(model=self.groundingdino,
+                                               images=image_trans,
+                                               captions=text_prompts,
+                                               box_threshold=box_threshold,
+                                               text_threshold=text_threshold,
+                                               device=self.device)
+        W, H = image_pils[0].size
+        boxes = [box_ops.box_cxcywh_to_xyxy(box) * torch.Tensor([W, H, W, H]) for box in boxes]
+
+        return boxes, logits, phrases
+
     def predict_sam(self, image_pil, boxes):
         image_array = np.asarray(image_pil)
         self.sam.set_image(image_array)
@@ -112,10 +125,74 @@ class LangSAM():
         )
         return masks.cpu()
 
+    def predict_sam_batch(self, image_pils, boxes):
+        images = [np.asarray(image_pil) for image_pil in image_pils]
+
+        def prepare_image(image):
+            if "RGB" != self.sam.model.image_format:
+                image = image[..., ::-1]
+            image = self.sam.transform.apply_image(image)   # (H, W, 3)
+            image = torch.as_tensor(image, device=self.device)
+            image = image.permute(2, 0, 1).contiguous()   # (3, H, W)
+            return image
+
+        def transform_boxes(image, boxes):
+            transformed_boxes = self.sam.transform.apply_boxes_torch(boxes, image.shape[:2])
+            return transformed_boxes
+
+        batched_input = []
+        for i in range(len(images)):
+            batched_input.append({
+                "image": prepare_image(images[i]).to(self.sam.device),
+                "boxes": transform_boxes(images[i], boxes[i]).to(self.sam.device),
+                "original_size": images[i].shape[:2],
+            })
+        batched_output = self.sam.model(batched_input, multimask_output=False)
+        masks = [o["masks"].cpu() for o in batched_output]  # Each mask output is (1, 1, H, W)
+        return masks
+
     def predict(self, image_pil, text_prompt, box_threshold=0.3, text_threshold=0.25):
         boxes, logits, phrases = self.predict_dino(image_pil, text_prompt, box_threshold, text_threshold)
         masks = torch.tensor([])
         if len(boxes) > 0:
             masks = self.predict_sam(image_pil, boxes)
             masks = masks.squeeze(1)
+        return masks, boxes, phrases, logits
+
+    def predict_batch(self, image_pil, text_prompt, box_threshold=0.3, text_threshold=0.25):
+        """
+        Args:
+            image_pil: list of PIL images
+            text_prompt: list of strings
+            box_threshold: float
+            text_threshold: float
+        Returns:
+            masks: list of tensors (one tensor per image; each tensor is (num_boxes, H, W))
+            boxes: list of tensors (one tensor per image; each tensor is (num_boxes, 4))
+            phrases: list of strings
+            logits: list of floats
+        """
+        assert len(image_pil) == len(text_prompt), "Must have same number of images and text prompts"
+        boxes, logits, phrases = self.predict_dino_batch(image_pil, text_prompt, box_threshold, text_threshold)
+
+        # Gather the box and image elements with boxes
+        batch_images = []
+        batch_boxes = []
+        for i in range(len(boxes)):
+            if len(boxes[i]) > 0:
+                batch_images += [image_pil[i]]
+                batch_boxes += [boxes[i]]
+        if len(batch_images) > 0:
+            print(f"Predicting SAM on {len(batch_boxes)} images")
+            batch_masks = self.predict_sam_batch(batch_images, batch_boxes)
+            batch_masks = [m.squeeze(1) for m in batch_masks]
+
+        # Masks will be a list of tensors
+        masks = []
+        for i in range(len(boxes)):
+            if len(boxes[i]) > 0:
+                masks += [batch_masks.pop(0)]
+            else:
+                masks += [torch.tensor([])]
+
         return masks, boxes, phrases, logits
